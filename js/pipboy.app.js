edb1b7ccfcefc1de7d1ba5f81acf4f6f110dad71
Kien Do <kienmarkdo@gmail.com>
2021-06-01 21:57:11 -0400

    finished adding jQuery objects for the weapons

---
+++ js/pipboy.app.js
@@ -0,0 +1,34 @@
+/* jQuery for the pip boy clone created by Kien Do. */
+
+$(document).ready(function() {
+    const weapons = [
+        {
+            "name": "44_pistol",
+            "damage": 48,
+            "fire_rate": 6,
+            "range": 119,
+            "accuracy": 66,
+            "weight": 4.2,
+            "value": 99
+        },
+        {
+            "name": "10mm_pistol",
+            "damage": 18,
+            "fire_rate": 46,
+            "range": 119,
+            "accuracy": 61,
+            "weight": 4.2,
+            "value": 53
+        },
+        {
+            "name": "assault_rifle",
+            "damage": 30,
+            "fire_rate": 40,
+            "range": 119,
+            "accuracy": 72,
+            "weight": 13.1,
+            "value": 144
+        },
+
+    ];
+});
